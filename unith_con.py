a853443e937bc2fe066add52d4f1260543bc8279
MohamedAttia <68583415+youssef362@users.noreply.github.com>
2025-04-30 13:44:10 +0300

    Update unith_con.py

--- unith_con.py
+++ unith_con.py
@@ -3,7 +3,7 @@ import json
 import requests
 app = Flask(__name__)
 HEADERS={'Content-Type': 'application/json'}
-BOT_WEBHOOK='https://app.graphlogic.ai/connector/webim/webim_message/4c1014de8c2d9128b0797c8de8754c13/bot_api_webhook'
+BOT_WEBHOOK='https://app.graphlogic.ai/connector/webim/webim_message/8c226b30abbb2710a4703721b7cba862/bot_api_webhook'
 WEBHOOK_SITE='https://webhook.site/b03c80bf-6ae0-4222-bd81-4725376b81a8'
 
 @app.route('/conversation/<user_id>/message', methods=['post'])
